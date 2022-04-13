6071380af310a1925ac97b355c95bb646687ac94
Tarmo Kople <tarmo@bolt.eu>
2022-04-13 12:04:08 +0300

    Initial import

---
+++ README.md
@@ -0,0 +1,68 @@
+# MySQL EC2 metadata plugin
+
+Exposes EC2 instance metadata using INFORMATION_SCHEMA.
+
+## Description
+
+MySQL EC2 metadata plugin downloads EC2 instance metada from metada service and exposes downloaded information in INFORMATION_SCHEMA.EC2_META table:
+
+```mysql> select * from EC2_META;
++--------------------------------+-----------------------------------------------+
+| METAOPT                        | VALUE                                         |
++--------------------------------+-----------------------------------------------+
+| placement/availability-zone    | eu-central-1b                                 |
+| placement/availability-zone-id | euc1-az3                                      |
+| placement/region               | eu-central-1                                  |
+| ami-id                         | ami-0a95c78892a8d8b50                         |
+| ami-launch-index               | 0                                             |
+| ami-manifest-path              | (unknown)                                     |
+| hostname                       | ip-172-29-55-67.eu-central-1.compute.internal |
+| instance-action                | none                                          |
+| instance-id                    | i-0482dce76cd95cf93                           |
+| instance-life-cycle            | on-demand                                     |
+| instance-type                  | m6i.xlarge                                    |
+| local-hostname                 | ip-172-29-55-67.eu-central-1.compute.internal |
+| local-ipv4                     | 172.29.55.67                                  |
+| mac                            | 06:ac:da:ec:90:00                             |
+| profile                        | default-hvm                                   |
+| reservation-id                 | r-09985cb3c47c494f3                           |
+| security-groups                | default                                       |
++--------------------------------+-----------------------------------------------+
+17 rows in set (0.00 sec)
+```
+
+## Getting Started
+
+### Dependencies
+
+* MySQL
+
+### Installing
+
+mysql> INSTALL PLUGIN EC2_META SONAME 'ec2_meta.so';
+
+### Executing program
+
+mysql> SELECT * FROM EC2_META;
+
+### Uninstalling
+
+mysql> UNINSTALL PLUGIN EC2_META;
+
+## Help
+
+Any advise for common problems or issues.
+
+## Author
+
+Tarmo Kople
+
+
+## Version History
+
+* 0.1
+  * Initial Release
+
+## License
+
+This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
---
+++ ec2_meta.cc
@@ -0,0 +1,129 @@
+#include <sql_class.h>
+#include <table.h>
+#include <string>
+#include <vector>
+#include <regex>
+#include "HTTPRequest.hpp"
+#include <mysql/service_my_plugin_log.h>
+
+static MYSQL_PLUGIN plugin_info_ptr;
+
+std::string ec2_meta_keys[] = {
+	"placement/availability-zone",
+	"placement/availability-zone-id",
+	"placement/region",
+	"ami-id",
+	"ami-launch-index",
+	"ami-manifest-path",
+	"hostname",
+	"instance-action",
+	"instance-id",
+	"instance-life-cycle",
+	"instance-type",
+	"local-hostname",
+	"local-ipv4",
+	"mac",
+	"profile",
+	"reservation-id",
+	"security-groups"
+};
+
+std::string ec2_meta_url = "http://169.254.169.254/latest/meta-data/";
+
+
+static struct st_mysql_information_schema ec2_meta_table_info =
+        {MYSQL_INFORMATION_SCHEMA_INTERFACE_VERSION};
+
+static ST_FIELD_INFO ec2_meta_table_fields[] =
+        {
+                {"METAOPT",   255, MYSQL_TYPE_STRING, 0, 0, 0, 0},
+                {"VALUE",     255, MYSQL_TYPE_STRING, 0, 0, 0, 0},
+                {0,       0,  MYSQL_TYPE_NULL,   0, 0, 0, 0}
+        };
+
+static int ec2_meta_fill_table(THD *thd, TABLE_LIST *tables, Item *cond) {
+    struct meta_row_t {
+        std::string key;
+        std::string value;
+    };
+    static std::vector <meta_row_t> ec2_meta_rows;
+    static bool ec2_meta_downloaded = false;
+    
+    if (!ec2_meta_downloaded) {
+        my_plugin_log_message(&plugin_info_ptr, MY_INFORMATION_LEVEL, "downloading ec2 meta info...");
+        for (uint i = 0; i < sizeof(ec2_meta_keys) / sizeof(*ec2_meta_keys); i++) {
+            try {
+                http::Request request{ec2_meta_url + ec2_meta_keys[i], http::InternetProtocol::V4};
+                const auto response = request.send("GET", "", {
+                        {"Content-Type", "application/x-www-form-urlencoded"},
+                        {"User-Agent",   "mysql-ec2-meta/0.1"},
+                        {"Accept",       "*/*"}
+                }, std::chrono::seconds(1));
+                if (response.status.code == http::Status::Ok) {
+                    std::string res = std::string{response.body.begin(), response.body.end()};
+                    res = std::regex_replace(res, std::regex("\\r$|\\n$"), "");
+
+                    ec2_meta_rows.push_back(meta_row_t{ec2_meta_keys[i], res});
+                    my_plugin_log_message(&plugin_info_ptr, MY_INFORMATION_LEVEL, "got info %s", ec2_meta_keys[i].c_str());
+                } else {
+                    continue;
+                }
+            }
+            catch (const http::RequestError& e)
+            {
+                my_plugin_log_message(&plugin_info_ptr, MY_ERROR_LEVEL, "request error: %s", e.what());
+            }
+            catch (const http::ResponseError& e)
+            {
+                my_plugin_log_message(&plugin_info_ptr, MY_ERROR_LEVEL, "response error: %s", e.what());
+
+            }
+            catch (const std::exception& e)
+            {
+                my_plugin_log_message(&plugin_info_ptr, MY_ERROR_LEVEL, "error: %s", e.what());
+            }
+        }
+        my_plugin_log_message(&plugin_info_ptr, MY_INFORMATION_LEVEL, "done.");
+        ec2_meta_downloaded = true;
+    }
+
+    TABLE *table = tables->table;
+
+    std::vector<meta_row_t>::iterator iter;
+        for(iter = ec2_meta_rows.begin(); iter != ec2_meta_rows.end(); ++iter){
+            table->field[0]->store(iter->key.c_str(), iter->key.length(), system_charset_info);
+            table->field[1]->store(iter->value.c_str(), iter->value.length(), system_charset_info);
+            if (schema_table_store_record(thd, table))
+                return 1;
+        }
+    return 0;
+}
+
+static int ec2_meta_table_init(MYSQL_PLUGIN plugin_info) {
+    plugin_info_ptr= plugin_info;
+
+    ST_SCHEMA_TABLE *schema_table = (ST_SCHEMA_TABLE *) plugin_info;
+
+    schema_table->fields_info = ec2_meta_table_fields;
+    schema_table->fill_table = ec2_meta_fill_table;
+    return 0;
+}
+
+
+mysql_declare_plugin(EC2_META)
+        {
+                MYSQL_INFORMATION_SCHEMA_PLUGIN,
+                &ec2_meta_table_info,                       /* type-specific descriptor */
+                "EC2_META",                                 /* table name */
+                "Tarmo Kople",                              /* author */
+                "Exposes EC2 instance metadata as table",   /* description */
+                PLUGIN_LICENSE_GPL,                         /* license type */
+                ec2_meta_table_init,                        /* init function */
+                NULL,
+                0x0100,                                     /* version = 1.0 */
+                NULL,                                       /* no status variables */
+                NULL,                                       /* no system variables */
+                NULL,                                       /* no reserved information */
+                0                                           /* no flags */
+        }
+mysql_declare_plugin_end;
